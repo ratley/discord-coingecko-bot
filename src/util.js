3e795843e7c4ba70db226953819920c31818a3d4
Bradley Inniss <bfinniss@gmail.com>
2021-04-23 01:24:53 -0400

    updated embed return

--- src/util.js
+++ src/util.js
@@ -13,18 +13,44 @@ export const createEmbed = ({
   author,
   rank,
   price,
+  pricePercent1h,
+  pricePercent24h,
+  pricePercent7d,
+  pricePercent14d,
+  pricePercent30d,
 }) => {
   const embed = new MessageEmbed()
-    .setTitle(`**${price}**`)
     .setColor(color)
     .setURL(url)
     .setAuthor(name, thumbnail)
-    // .setImage(image)
     .setThumbnail(thumbnail)
-    .addFields({ name: "Market Cap", value: `**Rank ${rank}**` })
-    .setTimestamp();
-
-  // .setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
+    .addFields(
+      {
+        name: `**Price**`,
+        value: price,
+      },
+      {
+        name: "**1H**",
+        value: `${Number(pricePercent1h).toFixed(2)}%`,
+        inline: true,
+      },
+      {
+        name: "**1D**",
+        value: `${Number(pricePercent24h).toFixed(2)}%`,
+        inline: true,
+      },
+      {
+        name: "**7D**",
+        value: `${Number(pricePercent7d).toFixed(2)}%`,
+        inline: true,
+      },
+      { name: "**Market Cap**", value: `Rank ${rank}` }
+    )
+    .setTimestamp()
+    .setFooter(
+      "Powered by Coingecko",
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fquiin.s3.us-east-1.amazonaws.com%2Forganizations%2Fpictures%2F000%2F004%2F638%2Foriginal%2FCoinGecko_Logo.png%3F1585529895&f=1&nofb=1"
+    );
 
   return embed;
 };
